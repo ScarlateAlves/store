8fa7f34284ae09f8e53cd845d65cf1b1f7b94746
ScarlateAlves <scarlate100@gmail.com>
2025-08-31 10:47:24 -0300

    chore: add link a

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,6 +3,12 @@ export default function Contact() {
     <div>
       <h3>Contato</h3>
       <a href="https://pixabay.com/pt/">Imagens gratuitas</a>
+
+      <a href="https://eventosti.dev/">Eventos fron</a>
+
+      <a href="https://thedevconf.com/tdc/2025/index.html">
+        https://thedevconf.com/tdc/2025/index.html
+      </a>
     </div>
   );
 }
