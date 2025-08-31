d0a71e370c00aca8001a20f63dd01b4774887817
ScarlateAlves <scarlate100@gmail.com>
2025-08-31 11:03:37 -0300

    feat: create hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,13 @@
 "use client";
 
+import { Hero } from "@/components/sections/Hero";
 import { Button } from "@/components/ui/Button";
 import { Heading } from "@/components/ui/Heading";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-      <div className="w-full">
-        <Image
-          className="object-cover max-h-[600px]"
-          src="/image-hero.jpg"
-          alt="Next.js logo"
-          layout="responsive"
-          width={300}
-          height={400}
-        />
-      </div>
+      <Hero title="Olá" description="Descrição" />
       <section className="max-w-7xl m-auto w-full py-20">
         <div className="flex gap-6">
           <Heading>Flash Sales</Heading>
